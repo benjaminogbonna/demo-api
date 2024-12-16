b9d2e07f4d0a3da0089ae0e8458b8f0a3061c305
Benjamin <onyedikachi1997@gmail.com>
2024-12-16 13:54:27 +0100

    update for actions.

--- demo_api/urls.py
+++ demo_api/urls.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
@@ -12,5 +13,10 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('chat-api/', include('main.urls')),
 
-    
+    # drf_spectacular
+    path('chat-api-auth/', include('rest_framework.urls')),
+    path('chat-api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('chat-api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('chat-api/redoc/docs/', SpectacularRedocView.as_view(url_name='api-schema'), name='redoc')
+
 ]
